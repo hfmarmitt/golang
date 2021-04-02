a05b81e04741d04c82b21b7e930fef3665f71c77
Heinrich Marmitt <hfmarmitt@gmail.com>
2021-04-02 13:27:03 -0300

    Exercises - Ninja Level 1

--- src/001-ninjalevel1/main.go
+++ src/001-ninjalevel1/main.go
@@ -2,6 +2,63 @@ package main
 
 import "fmt"
 
+var x int = 42
+var y string = "James Bond"
+var z bool = true
+var underlyingTypeVar int
+
 func main() {
-	fmt.Println("Test")
+	handsOnOne()
+	handsOnTwo()
+	handsOnThree()
+	handsOnFour()
+	handsOnFive()
+}
+
+func handsOnOne() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Printf("%d\n", x)
+	fmt.Printf(y + "\n")
+	fmt.Println(z)
+
+}
+
+func handsOnTwo() {
+	fmt.Println(x, y, z)
+	fmt.Printf("%d\n", x)
+	fmt.Printf(y + "\n")
+	fmt.Println(z)
+}
+
+func handsOnThree() {
+	s := fmt.Sprintf("%d - %s - %t\n", x, y, z)
+	fmt.Print(s)
+}
+
+func handsOnFour() {
+	type customType int
+
+	var x customType
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+}
+
+func handsOnFive() {
+	type customType int
+
+	var x customType
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+	underlyingTypeVar = int(x)
+	fmt.Println(underlyingTypeVar)
 }
